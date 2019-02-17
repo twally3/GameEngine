f840dbd82b6b5771c1535d84d0aa40642936110a
Max Taylor <max@cakerstream.com>
2019-02-17 01:47:18 +0000

    Added shaders to change the quad colour

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -16,6 +17,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {
 			-0.5f, 0.5f, 0f,
@@ -33,10 +35,13 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
---
+++ src/shaders/StaticShader.java
@@ -0,0 +1,14 @@
+package shaders;
+
+public class StaticShader extends ShaderProgram{
+	private static final String VERTEX_FILE = "src/shaders/vertexShader";
+	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader";
+	
+	public StaticShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+	
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+	}
+}
