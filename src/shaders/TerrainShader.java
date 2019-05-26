b265ab44087217244ba931cdec0bff50a0ed0213
Max Taylor <max@cakerstream.com>
2019-05-26 18:25:39 +0100

    Added support for fog

--- src/shaders/TerrainShader.java
+++ src/shaders/TerrainShader.java
@@ -1,6 +1,7 @@
 package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Light;
@@ -17,6 +18,7 @@ public class TerrainShader extends ShaderProgram {
 	private int location_lightColour;
 	private int location_shineDamper;
 	private int location_reflectivity;
+	private int location_skyColour;
 	
 	public TerrainShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -37,6 +39,7 @@ public class TerrainShader extends ShaderProgram {
 		location_lightColour = super.getUniformLocation("lightColour");
 		location_shineDamper = super.getUniformLocation("shineDamper");
 		location_reflectivity = super.getUniformLocation("reflectivity");
+		location_skyColour = super.getUniformLocation("skyColour");
 	}
 	
 	public void loadTransformationMatrix(Matrix4f matrix) {
@@ -60,5 +63,9 @@ public class TerrainShader extends ShaderProgram {
 	public void loadShineVariables(float damper, float reflectivity) {
 		super.loadFloat(location_shineDamper, damper);
 		super.loadFloat(location_reflectivity, reflectivity);
+	}
+	
+	public void loadSkyColour(float r, float g, float b) {
+		super.loadVector(location_skyColour, new Vector3f(r, g, b));
 	}
 }
