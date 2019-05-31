efbe7a334881b4cd1e0e9e6c76e2a27e22d31736
Max Taylor <max@cakerstream.com>
2019-05-31 15:28:30 +0100

    Added ability to use a blend map with terrain for multitexturing

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -15,6 +15,8 @@ import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 
 public class MainGameLoop {
 
@@ -24,6 +26,13 @@ public class MainGameLoop {
 		
 		
 		Loader loader = new Loader();
+		
+		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("dirt"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("pinkFlowers"));
+		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
+		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
 		
 		ModelData data = OBJFileLoader.loadOBJ("dragon");
 		RawModel model = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), data.getNormals(), data.getIndices());
@@ -35,8 +44,8 @@ public class MainGameLoop {
 		Entity entity = new Entity(staticModel, new Vector3f(50, 0, -50), 0, 0, 0, 1);
 		Light light = new Light(new Vector3f(300, 2000, 2000), new Vector3f(1,1,1));;
 		
-		Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
-		Terrain terrain2 = new Terrain(1, -1, loader, new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap);
+		Terrain terrain2 = new Terrain(-1, -1, loader, texturePack, blendMap);
 		
 		Camera camera = new Camera();
 		camera.setPosition(new Vector3f(0.0f, 5.0f, 0.0f));
@@ -52,7 +61,18 @@ public class MainGameLoop {
 		Entity[] fernEntities = new Entity[400];
 		
 		for (int i = 0; i < fernEntities.length; i++) {
-			fernEntities[i] = new Entity(fernStaticModel, new Vector3f((float) Math.random() * 1600, 0, (float) Math.random() * -800), 0, 0, 0, 1); 
+			fernEntities[i] = new Entity(fernStaticModel, new Vector3f((float) Math.random() * 1600 - 800, 0, (float) Math.random() * -800), 0, 0, 0, 1); 
+		}
+		
+		ModelData treeModelData = OBJFileLoader.loadOBJ("tree");
+		RawModel treeModel = loader.loadToVAO(treeModelData.getVertices(), treeModelData.getTextureCoords(), treeModelData.getNormals(), treeModelData.getIndices());
+		TexturedModel treeStaticModel = new TexturedModel(treeModel, new ModelTexture(loader.loadTexture("tree")));
+		ModelTexture treeTexture = treeStaticModel.getTexture();
+		
+		Entity[] treeEntities = new Entity[200];
+		
+		for (int i = 0; i < treeEntities.length; i++) {
+			treeEntities[i] = new Entity(treeStaticModel, new Vector3f((float) Math.random() * 1600 - 800, 0, (float) Math.random() * -800), 0, 0, 0, 10); 
 		}
 		
 		while(!Display.isCloseRequested()) {
@@ -61,6 +81,10 @@ public class MainGameLoop {
 			renderer.processTerrain(terrain);
 			renderer.processTerrain(terrain2);
 			renderer.processEntity(entity);
+			
+			for (Entity treeEntity : treeEntities) {
+				renderer.processEntity(treeEntity);
+			}
 			
 			for (Entity fernEntity : fernEntities) {
 				renderer.processEntity(fernEntity);
